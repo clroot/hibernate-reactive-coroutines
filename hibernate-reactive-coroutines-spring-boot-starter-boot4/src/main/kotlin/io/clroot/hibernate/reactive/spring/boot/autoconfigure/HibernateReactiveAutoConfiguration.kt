fc30d30050393de7e1e9dc1092416e93b5d373c3
clroot <geonhwan.cha@clroot.io>
2026-07-23 17:14:29 +0900

    fix: sanitize PostgreSQL reactive URLs (#12)

--- hibernate-reactive-coroutines-spring-boot-starter-boot4/src/main/kotlin/io/clroot/hibernate/reactive/spring/boot/autoconfigure/HibernateReactiveAutoConfiguration.kt
+++ hibernate-reactive-coroutines-spring-boot-starter-boot4/src/main/kotlin/io/clroot/hibernate/reactive/spring/boot/autoconfigure/HibernateReactiveAutoConfiguration.kt
@@ -95,7 +95,7 @@ class HibernateReactiveAutoConfiguration(
     @Bean
     @ConditionalOnMissingBean(name = ["hibernateSessionFactory"])
     fun hibernateSessionFactory(): org.hibernate.SessionFactory {
-        val reactiveUrl = jdbcUrl.replace("jdbc:", "")
+        val reactiveUrl = ReactiveConnectionUrl.fromJdbc(jdbcUrl)
 
         val configuration =
             Configuration().apply {
